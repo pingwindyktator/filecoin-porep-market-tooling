c0beadc9b5fb1fc78f8c2c9242591af3a8ce3d48
pingwindyktator <pingwindyktator@pm.me>
2026-04-29 21:56:38 +0200

    Add sp is-authorized script

---
+++ cli/commands/sp/is_authorized.py
@@ -0,0 +1,17 @@
+import click
+
+from cli import utils
+from cli.commands.sp._sp import sp_address
+from cli.services.contracts.sp_registry import SPRegistry
+
+
+@click.command()
+@click.argument("provider_id")
+def is_authorized(provider_id: str):
+    """
+    Check if your private key is authorized to manage deals for the given provider ID.
+
+    PROVIDER_ID - Storage provider ID to check authorization for.
+    """
+
+    click.echo(SPRegistry().is_authorized_for_provider(sp_address(), utils.f0_str_id_to_int(provider_id)))
